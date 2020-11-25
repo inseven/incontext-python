0462710cac70942142d2a7aeb642c114b3b881d6
Jason Barrie Morley <jbmorley@mac.com>
2020-11-24 22:31:31 -0800

    Update the copyright notice.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2016-2020 Jason Barrie Morley
+# Copyright (c) 2016-2020 InSeven Limited
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
