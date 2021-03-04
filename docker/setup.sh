056ac3d281a03563f03ed3f9918a0fe99c21c242
Jason Barrie Morley <jbmorley@mac.com>
2021-03-04 13:30:34 -0800

    Simplify the docker build process.

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Install the package dependencies.
+cd $INCONTEXT_DIR
+apt-get update
+cat requirements.txt | xargs apt-get install -y
+
+# Timezone support
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+# Install ImageMagick with HEIC support
 sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
 apt-get update
 apt-get install -y build-essential autoconf git-core wget
@@ -10,18 +24,28 @@ git clone https://github.com/strukturag/libheif.git
 cd libde265/
 ./autogen.sh
 ./configure
-make –j4
+make
 make install
 cd /usr/src/libheif/
 ./autogen.sh
 ./configure
-make –j4
+make
 make install
 cd /usr/src/
 wget https://www.imagemagick.org/download/ImageMagick.tar.gz
 tar xf ImageMagick.tar.gz
+rm ImageMagick.tar.gz
 cd ImageMagick-7*
 ./configure --with-heic=yes
-make –j4
+make
 make install
 ldconfig
+
+# Install the Python dependencies.
+cd $INCONTEXT_DIR
+python3 -m pip install pipenv
+pipenv install --verbose --system --skip-lock
+
+# Clean up the working directory.
+cd /
+rm -r $INCONTEXT_DIR
