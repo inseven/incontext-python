236eec5e8c2071f3dbcd2a802d676cfb08619996
Jason Morley <jbmorley@mac-mini-build-02.local>
2021-08-25 17:35:44 -0700

    HEIF support seems like it might be working, but maybe tiff isn't?

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,84 +2,61 @@
 
 set -e
 set -x
-
-echo "HELLO!"
+set -o pipefail
+set -u
 
 # Install the package dependencies.
 cd $INCONTEXT_DIR
-cat requirements.txt | xargs apk add --no-cache 
+cat requirements.txt | xargs apk add --no-cache
 
 # Timezone support
 #ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 #dpkg-reconfigure --frontend noninteractive tzdata
 
-#mkdir -p /build-deps
-
-# # libde265
-# RUN set -ex \
-#     && cd /build-deps \
-#     && LIBDE265_VERSION="1.0.5" \
-#     && wget https://github.com/strukturag/libde265/releases/download/v${LIBDE265_VERSION}/libde265-${LIBDE265_VERSION}.tar.gz \
-#     && tar xvf libde265-${LIBDE265_VERSION}.tar.gz \
-#     && cd libde265-${LIBDE265_VERSION} \
-#     && ./autogen.sh \
-#     && ./configure --prefix /usr \
-#     && make -j4 \
-#     && make install \
-#     && ldconfig
-
-# # libheif
-# RUN set -ex \
-#     && cd /build-deps \
-#     && LIBHEIF_VERSION="1.7.0" \
-#     && wget https://github.com/strukturag/libheif/releases/download/v${LIBHEIF_VERSION}/libheif-${LIBHEIF_VERSION}.tar.gz \
-#     && tar xvf libheif-${LIBHEIF_VERSION}.tar.gz \
-#     && cd libheif-${LIBHEIF_VERSION} \
-#     && ./configure --prefix /usr \
-#     && make -j4 \
-#     && make install \
-#     && ldconfig
-
-# # libffi
-# RUN set -ex \
-#     && cd /build-deps \
-#     && LIBFFI_VERSION="3.3" \
-#     && wget ftp://sourceware.org/pub/libffi/libffi-${LIBFFI_VERSION}.tar.gz \
-#     && tar xvf libffi-${LIBFFI_VERSION}.tar.gz \
-#     && cd libffi-${LIBFFI_VERSION} \
-#     && ./configure --prefix /usr \
-#     && make -j4 \
-#     && make install \
-#     && ldconfig
-
-# Install ImageMagick with HEIC support
-# sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
-# apt-get update
-# apt-get install -y build-essential autoconf git-core wget
-# apt-get build-dep -y imagemagick libde265 libheif
-# cd /usr/src/
-# git clone https://github.com/strukturag/libde265.git
-# git clone https://github.com/strukturag/libheif.git
-# cd libde265/
-# ./autogen.sh
-# ./configure
-# make
-# make install
-# cd /usr/src/libheif/
-# ./autogen.sh
-# ./configure
-# make
-# make install
 mkdir -p /usr/src/
+
+LIBDE265_COMMIT=43c490812a6c7b78e9d73125c7e4e2d6ee9826d2
+LIBHEIF_COMMIT=0c49d5b4882bcbfc0279dede005832367eb83397
+IMAGEMAGICK_COMMIT=fa87fa7287c8275f52b508770c814815ebe61a02
+
+# libde265
 cd /usr/src/
-wget https://www.imagemagick.org/download/ImageMagick.tar.gz
-tar xf ImageMagick.tar.gz
-rm ImageMagick.tar.gz
-cd ImageMagick-7*
-./configure --with-heic=yes --with-tiff=yes
-make
-make install
-# ldconfig
+git clone -b frame-parallel https://github.com/strukturag/libde265.git
+cd libde265
+git checkout ${LIBDE265_COMMIT} .
+./autogen.sh
+./configure
+make -j8 install
+
+# libheif
+cd /usr/src/
+git clone https://github.com/strukturag/libheif.git
+cd libheif
+git checkout ${LIBHEIF_COMMIT} .
+./autogen.sh
+./configure
+make -j8 install
+
+# ImageMagick
+cd /usr/src/
+git clone https://github.com/ImageMagick/ImageMagick.git
+cd ImageMagick
+git checkout ${IMAGEMAGICK_COMMIT} .
+./configure \
+    --with-heic \
+    --with-tiff \
+    --with-heic \
+    --with-jpeg \
+    --with-lcms2 \
+    --with-png \
+    --with-gslib \
+    --with-openexr \
+    --with-zlib \
+    --with-gs-font-dir=/usr/share/fonts/Type1 \
+    --with-threads \
+    --with-webp \
+    --without-x
+make -j8 install
 
 # Install the Python dependencies.
 cd $INCONTEXT_DIR
@@ -88,6 +65,7 @@ pip3 install git+https://github.com/david-poirier-csn/pyheif.git
 pipenv install --verbose --system --skip-lock
 
 # Install the Ruby dependencies.
+# TODO: Ruby SASS is deprecated and should be removed.
 gem install sass
 
 # Clean up the working directory.
