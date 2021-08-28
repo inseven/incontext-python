2560d66556c5c4c13feac129e15ca5e64a5811d0
Jason Barrie Morley <jbmorley@mac.com>
2021-08-27 20:54:39 -0700

    feat: Skip the Docker container cache and force rebuild with `--no-cache`

--- cli.py
+++ cli.py
@@ -37,6 +37,7 @@ def parser():
     parser.add_argument('--site', '-s', default=os.getcwd(), help="path to the root of the site")
     parser.add_argument('--verbose', '-v', action='store_true', default=False, help="show verbose output")
     parser.add_argument('--volume', action='append', help="mount an additional volume in the Docker container")
+    parser.add_argument('--no-cache', action='store_true', default=False, help="skip Docker container cache")
     return parser
 
 
