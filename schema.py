3c6653067cb067cb59170eca0ae35e9ccd162378
Jason Morley <jbmorley@mac.com>
2022-07-30 03:50:58 -0700

    docs: Update the copyright year to 2022 (#183)

--- schema.py
+++ schema.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2016-2021 InSeven Limited
+# Copyright (c) 2016-2022 InSeven Limited
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
