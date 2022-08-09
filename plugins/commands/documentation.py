e64fd1dbbea1aabc1e710c499dc39c774f274a13
Jason Barrie Morley <jbmorley@mac.com>
2022-08-09 07:02:13 +0100

    fix: Drop the unnecessary dependency on Pipenv Docker builds have been failing as Pipenv no longer cleanly installs though in our Docker container. Since we'd already been planning to drop Pipenv (see https://github.com/inseven/incontext/issues/143), this change makes that switch to sidestep the Pipenv installation issue.

--- plugins/commands/documentation.py
+++ plugins/commands/documentation.py
@@ -62,6 +62,13 @@ def command_documentation(incontext, options):
         files = utils.find(paths.INCONTEXT_DIRECTORY,
                            extensions=[".py"],
                            transform=lambda x: os.path.relpath(x, paths.INCONTEXT_DIRECTORY))
+
+        # Don't attempt to generate documentation for website.py as Flask gets upset.
+        # TODO: Restore the filter documentation generation after dropping Flask dependency #188
+        #       https://github.com/inseven/incontext/issues/188
+        files.remove("service/website.py")
+
+        # Get the paths as Python module names.
         modules = [os.path.splitext(f)[0] for f in files]
 
         # Generate the Python documentation.
