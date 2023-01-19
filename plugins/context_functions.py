dc360333a89a7bbbff65d15a5e73f85462eb2b8e
Jason Morley <hello@jbmorley.co.uk>
2023-01-19 08:54:18 +0000

    fix: Update the copyright notice to 2023 (#198)

--- plugins/context_functions.py
+++ plugins/context_functions.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2021 Jason Barrie Morley
+# Copyright (c) 2016-2023 Jason Barrie Morley
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
